82a7a0200f2dca7e268d6aca6453882a908bd52c
Bruno Rainha <brunorainha01@gmail.com>
2025-01-09 19:55:35 -0300

    Add API connection logic

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Crypto Data App - Início do Projeto")
+
+import requests
+
+API_URL = "https://api.coincap.io/v2/assets"
+
+def fetch_data():
+    try:
+        response = requests.get(API_URL)
+        response.raise_for_status()
+        return response.json()
+    except Exception as e:
+        print(f"Error fetching data: {e}")
+
+if __name__ == "__main__":
+    data = fetch_data()
+    print(data)
